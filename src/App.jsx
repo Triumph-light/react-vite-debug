6082477d4488c403cf2ff59b30a3879e3e6bf03a
Triumph-light <2495685883@qq.com>
2025-05-12 23:19:06 +0800

    feat: init state condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Counter from "./Counter";
+import React,{ useState } from "react";
 
 export default function App() {
+  const [count, setCount] = useState(0)
   return (
     <>
+      {count}
       <h1>Hello World</h1>
-      <Counter />
+      <Button onClick={() => setCount(pre => pre +1)}>click</Button>
     </>
   );
 }
